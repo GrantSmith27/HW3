532fc17a667029678170b9d8cf9d0b501d4c368a
gsmith27 <gsmith12@luc.edu>
2024-10-02 11:06:19 -0500

    done with AVLTree

--- TreeProblems.java
+++ TreeProblems.java
@@ -7,6 +7,8 @@
  *
  */
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class TreeProblems {
@@ -27,7 +29,21 @@ public class TreeProblems {
     // *several* lines of code. Hint: create two temporary TreeSets and utilize the
     // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
 
-    return setA;
+    TreeSet<Integer> TempSet1 = new TreeSet<>();
+    TreeSet<Integer> TempSet2 = new TreeSet<>();
+
+    //Have one set have all the values
+    TempSet1.addAll(setA);
+    TempSet1.addAll(setB);
+
+    //Have the other set have all the common values
+    TempSet2.addAll(setB);
+    TempSet2.retainAll(setA);
+
+    //subtract them from each other
+    TempSet1.removeAll(TempSet2);
+
+    return TempSet1;
   }
 
 
